c585f0b276a8fb5baffa6d3a526a27b3eefdd275
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-06 02:43:00 +0000

    Add State Push Pop and Change functions to Game

--- arduboy-pokemon-thing/Game.cpp
+++ arduboy-pokemon-thing/Game.cpp
@@ -9,3 +9,32 @@ void Game::loop()
 {
 	
 }
+
+
+
+//States
+GameState Game::StateConstruct(const GameStateType state)
+{
+	switch(state)
+	{
+		default:
+			return GameState();
+		break;
+	}
+}
+
+void Game::StateChange(const GameStateType stateNext)
+{
+	this->stateStack.clear();
+	PushState(stateNext);
+}
+
+void Game::PopState()
+{
+	this->stateStack.drop();
+}
+
+void Game::PushState(const GameStateType stateNew)
+{
+	this->stateStack.push(StateConstruct(stateNew));
+}
--- arduboy-pokemon-thing/Game.h
+++ arduboy-pokemon-thing/Game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Arduboy2.h>
 #include "Utilities/Stack.h"
+
 #include "GameStateType.h"
 #include "GameState.h"
 
@@ -8,8 +9,15 @@ class Game
 {
 private:
 	Arduboy2 arduboy;
+	
 	Stack<GameState, 4> stateStack;
+	
+	GameState StateConstruct(const GameStateType state);
 public:
 	void setup();
 	void loop();
+	
+	void StateChange(const GameStateType stateNext);	//kills entire stack and replaces position 0
+	void PushState(const GameStateType stateNew);		//adds to stack
+	void PopState();
 };
