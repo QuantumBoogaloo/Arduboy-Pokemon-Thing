51e001e9db880ba2e8262c07d09f3fdaa7b6a4b2
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-06 02:42:33 +0000

    Construct Stack<GameState> in Game.h
    i know what i'm doing

--- arduboy-pokemon-thing/Game.h
+++ arduboy-pokemon-thing/Game.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <Arduboy2.h>
+#include "Utilities/Stack.h"
+#include "GameStateType.h"
+#include "GameState.h"
 
 class Game
 {
 private:
 	Arduboy2 arduboy;
+	Stack<GameState, 4> stateStack;
 public:
 	void setup();
 	void loop();
