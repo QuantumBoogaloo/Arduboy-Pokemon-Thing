2bc7384145968a0b3aa4be54838dc0507c74ec03
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-10 02:00:35 +0000

    Remove unneeded includes and functions in Game

--- arduboy-pokemon-thing/Game.h
+++ arduboy-pokemon-thing/Game.h
@@ -1,26 +1,14 @@
 #pragma once
 #include <Arduboy2.h>
-#include "Utilities/Stack.h"
 
-#include "GameStateType.h"
-#include "GameState.h"
-
-#include "MainMenuState.h"
-#include "StartupState.h"
 #include "StateMachine.h"
 
 class Game
 {
 private:
 	Arduboy2 arduboy;
-	
-	Stack< GameState *, 4> stateStack;
 	StateMachine states;
 public:
 	void setup();
 	void loop();
-	
-	void statePush(const GameStateType stateNew);
-	void stateDrop();
-	void stateDropAll();
 };
