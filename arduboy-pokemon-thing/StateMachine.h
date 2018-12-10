ec505e9cfcc17d8b07cb4c24f5650c5d79d05f23
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-10 01:56:42 +0000

    Create StateMachine class

---
+++ arduboy-pokemon-thing/StateMachine.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Utilities/Stack.h"
+
+#include "GameStateType.h"
+#include "GameState.h"
+#include "StateMachineInstruction.h"
+
+#include "MainMenuState.h"
+#include "StartupState.h"
+
+class StateMachine
+{
+private:
+	void stateDrop();
+public:
+};
