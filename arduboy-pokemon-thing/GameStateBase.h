f9d3b60da914f21dc0f1480deb183876a6d4b901
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-15 12:07:53 +0000

    Add constructor, destructor, update and draw functions to GameStateBase

--- arduboy-pokemon-thing/GameStateBase.h
+++ arduboy-pokemon-thing/GameStateBase.h
@@ -1,7 +1,13 @@
 #pragma once
+#include <Arduboy2.h>
+#include "UIContainer.h"
 
 class GameStateBase 
 {
 private:
 public:
+	GameStateBase() {}
+	virtual ~GameStateBase() {}
+	virtual void update(Arduboy2 & arduboy, UIContainer & uiContainer) {};
+	virtual void draw(Arduboy2 & arduboy) {};
 };
