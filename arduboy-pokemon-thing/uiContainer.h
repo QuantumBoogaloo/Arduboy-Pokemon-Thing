d1e156a2a9494622583569bc5ca66f98bac45822
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-15 01:48:40 +0000

    Add stack of UIObjectBase pointers to uiContainer

--- arduboy-pokemon-thing/uiContainer.h
+++ arduboy-pokemon-thing/uiContainer.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "Utilities/Stack.h"
+#include "UIObjectBase.h"
 
 class UIContainer
 {
 private:
+	static constexpr uint8_t stackSize = 4;
+	Stack<UIObjectBase *, stackSize> uiStack;
 public:
 };
