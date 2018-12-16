a2fd7c3663db134ac2f4c6c26c687b7ff8512953
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-16 01:06:13 +0000

    Add GameState variables to Game.h

--- arduboy-pokemon-thing/Game.h
+++ arduboy-pokemon-thing/Game.h
@@ -1,12 +1,19 @@
 #pragma once
 #include <Arduboy2.h>
+
 #include "UIContainer.h"
+#include "GameStateBase.h"
+#include "GameStateType.h"
 
 class Game
 {
 private:
 	Arduboy2 arduboy;
 	UIContainer uiContainer;
+	
+	GameStateBase * state;
+	GameStateType currentState = GameStateType::None;
+	GameStateType nextState = GameStateType::Menu;
 public:
 	void begin();
 	void update();
