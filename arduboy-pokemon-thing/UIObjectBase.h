fdeb2c22349e13f2da38cabd1069c3f1b13ad0c4
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:23:50 +0000

    Add UICommand and setters to UIObjectBase

--- arduboy-pokemon-thing/UIObjectBase.h
+++ arduboy-pokemon-thing/UIObjectBase.h
@@ -3,14 +3,26 @@
 #include "UIContainer.h"
 
 class UIContainer;
+#include "GameStateType.h"
+#include "UIStateType.h"
+#include "UICommand.h"
 
 class UIObjectBase
 {
-private:
+protected:
+	UICommand command;
 public:
 	UIObjectBase() {}
 	virtual ~UIObjectBase() {}
 	virtual void update(Arduboy2 & arduboy) {};
 	virtual void draw(Arduboy2 & arduboy) {};
 	virtual void write(uint8_t letter) {};
+	UICommand & getCommand()
+	{
+		return this->command;
+	}
+	void resetCommand()
+	{
+		this->command = UICommand();
+	}
 };
