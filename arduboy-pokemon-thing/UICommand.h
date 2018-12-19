7cf7d3673978dccabfde72334cba28a6c8788651
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:22:42 +0000

    Add constructors to UICommand

--- arduboy-pokemon-thing/UICommand.h
+++ arduboy-pokemon-thing/UICommand.h
@@ -30,5 +30,28 @@ private:
 		GameStateType stateData;
 	} data;
 public:
+	UICommand()
+	{
+		this->command = UICommandType::None;
+		this->target = UICommandTarget::None;
+	}
+	UICommand(UICommandType command, UIStateType data)
+	{
+		this->command = command;
+		this->target = UICommandTarget::UI;
+		this->data.uiData = data;
+	}
+	UICommand(UICommandType command, GameStateType data)
+	{
+		this->command = command;
+		this->target = UICommandTarget::State;
+		this->data.stateData = data;
+	}
+	UICommand(UICommandType command, UICommandTarget target)
+	{
+		this->command = command;
+		this->target = target;
+	}
+		
 };
 
