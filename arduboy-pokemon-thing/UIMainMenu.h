979ef95aa769246b2b249a5dea8f56524afef2b2
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:29:28 +0000

    Add quick and dirty Main menu implementation

--- arduboy-pokemon-thing/UIMainMenu.h
+++ arduboy-pokemon-thing/UIMainMenu.h
@@ -9,5 +9,33 @@
 class UIMainMenu : public UIObjectBase
 {
 private:
+	UIMenuBase menu = UIMenuBase(MainMenuItems);
+	
 public:
+	UIMainMenu(StandardMessageBuffer & messageBuffer) : UIObjectBase(messageBuffer) {}
+	
+	void update(Arduboy2 & arduboy)
+	{
+		if(arduboy.justPressed(UP_BUTTON))
+		{
+			menu.selectPreviousIndex();
+		}
+		if(arduboy.justPressed(DOWN_BUTTON))
+		{
+			menu.selectNextIndex();
+		}
+		if(arduboy.justPressed(A_BUTTON))
+		{
+			command = UICommand(UICommandType::Push, UIStateType::Textbox);
+			print(F("holy shit this works!"));
+		}
+	}	
+	void draw(Arduboy2 & arduboy)
+	{
+		for(uint8_t i = 0; i < menu.getEndIndex(); ++i)
+		{
+			arduboy.setCursor(0, i * 8);
+			arduboy.print(menu.getString(i));
+		}
+	}
 };
