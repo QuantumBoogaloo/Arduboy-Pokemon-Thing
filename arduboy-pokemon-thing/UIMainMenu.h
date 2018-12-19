818431d7954c9717fd32374b479f98a708f4eea7
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:29:13 +0000

    Create UIMainMenu.h

---
+++ arduboy-pokemon-thing/UIMainMenu.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "UIObjectBase.h"
+#include "UICommand.h"
+
+#include "UIMenuBase.h"
+#include "Data/MenuDefines.h"
+
+class UIMainMenu : public UIObjectBase
+{
+private:
+public:
+};
