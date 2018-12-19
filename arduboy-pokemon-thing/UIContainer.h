062499bfe5cedb91f304b93f8fd19ccbe96d6efa
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:27:08 +0000

    Tidy UIContainer class

--- arduboy-pokemon-thing/UIContainer.h
+++ arduboy-pokemon-thing/UIContainer.h
@@ -4,22 +4,23 @@
 #include "UIMessageBuffer.h"
 #include "UIObjectBase.h"
 
-#include "UITextbox.h"
 #include "UIStateType.h"
+#include "UITextbox.h"
+#include "UIMainMenu.h"
 
 class UIContainer 
 {
 private:
 	static constexpr uint8_t stackSize = 4;
+	
 	Stack<UIObjectBase *, stackSize> uiStack;
 	Stack<UIStateType, stackSize> typeStack;
-public:
-	void update() {}
-	
 	StandardMessageBuffer messageBuffer;
+public:	
 	void push(const UIStateType type);
 	void drop();
 	void dropAll();
+	void update();
 	
 	uint8_t getCount() const;
 	bool isEmpty() const;
