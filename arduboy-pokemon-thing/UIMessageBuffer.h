109891a73bdab66f16ed7be4bdd43d22e468a526
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:09:19 +0000

    Add clear() function to UIMessageBuffer

--- arduboy-pokemon-thing/UIMessageBuffer.h
+++ arduboy-pokemon-thing/UIMessageBuffer.h
@@ -8,6 +8,13 @@ private:
 	char buffer[size];
 	uint8_t position = 0;
 public:
+	{
+		for(uint8_t i = 0; i < size; ++i)
+		{
+			buffer[i] = '\0';
+		}
+		position = 0;
+	}
 	void write(const char letter)
 	{
 		buffer[position] = letter;
