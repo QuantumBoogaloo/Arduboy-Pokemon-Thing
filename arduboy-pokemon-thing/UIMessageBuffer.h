df7fcb9bd438b8e934e8b7835e78b715baef2eb5
bakagamedev <noel.abercrombie@hotmail.com>
2018-12-19 01:09:10 +0000

    Add write(char) function to UIMessageBuffer

--- arduboy-pokemon-thing/UIMessageBuffer.h
+++ arduboy-pokemon-thing/UIMessageBuffer.h
@@ -8,6 +8,12 @@ private:
 	char buffer[size];
 	uint8_t position = 0;
 public:
+	void write(const char letter)
+	{
+		buffer[position] = letter;
+		if(position < size)
+			++position;
+	}
 };
 
 using StandardMessageBuffer = UIMessageBuffer<40>;
